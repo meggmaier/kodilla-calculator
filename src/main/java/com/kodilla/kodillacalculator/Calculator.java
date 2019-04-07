fde9b210e998b3ee90fe7885828c19fb3d764645
meggmaier <megg.maier@gmail.com>
2019-04-07 18:02:56 +0200

    Calculator

---
+++ src/main/java/com/kodilla/kodillacalculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla.kodillacalculator;
+
+public class Calculator {
+    private double a;
+    private double b;
+
+    public double add(){
+        return a+b;
+    }
+    public double subtract(){
+        return a-b;
+    }
+}
+
+
